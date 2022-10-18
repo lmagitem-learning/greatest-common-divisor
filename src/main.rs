3eb4e936cac7a1795b9cc1cd4467679fa431da9f
Luc Magitem <31952766+lmagitem@users.noreply.github.com>
2022-10-18 22:14:27 +0200

    Added serde

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
 
 fn main() {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(get_index))
+        .route("/", web::get().to(get_index))
+        .route("/gcd", web::post().to(post_gcd))
     });
 
-    println!("Serving on http://localhost:8989...");
+    println!("Serving on http://localhost:3000...");
     server
-        .bind("localhost::8989").expect("Error binding server to address")
+        .bind("127.0.0.1::3000").expect("Error binding server to address")
         .run().expect("Error running server");
 }
 
@@ -25,6 +33,21 @@ fn get_index() -> HttpResponse {
                     </form>
                 "#
                 )
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.")
+    }
+
+    let response = format!("The greatest common divisor of {} and {} is <b>{}</b>.\n",
+                           form.n, form.m, gcd(form.n, form.m));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
